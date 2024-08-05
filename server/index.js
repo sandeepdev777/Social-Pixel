f0501e0459b774c4057b94fea0e0f01061be4d15
Sandeep Kumar <sandeepkumar07007@gmail.com>
2024-08-05 19:33:10 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -25,7 +25,10 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 
-
+app.use(cors({
+    origin:"*",
+    methods: ["GET","POST","PUT","DELETE"],
+}))
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
