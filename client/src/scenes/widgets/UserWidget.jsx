aac105b84014a0f6e426f0492a050aa568a5c2fa
Sandeep Kumar <sandeepkumar07007@gmail.com>
2024-08-05 19:10:58 +0530

    fix

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -23,17 +23,13 @@ const UserWidget=({userId,picturePath})=>{
 
 
 // getting the user details after just logging in
-const getUser=async()=>{
-    const response=await fetch(`http://localhost:3001/users/${userId}`,{
-        method:"GET",
-        headers:{ Authorization:`Bearer ${token}`}
-        })
-        const data=await response.json();
-        setUser(data);
-    };
-
-    useEffect(()=>{ // this renders the user details when the user logs in
-        getUser();
+    useEffect(async ()=>{ // this renders the user details when the user logs in
+        const response=await fetch(`http://localhost:3001/users/${userId}`,{
+            method:"GET",
+            headers:{ Authorization:`Bearer ${token}`}
+            })
+            const data=await response.json();
+            setUser(data);
     },[]);
 
      if(!user) return null;
